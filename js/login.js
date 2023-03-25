bba675a397ea51ac9cd3e0da7d69f4a290c0c1c7
henry robbet <116162120+hernyrobbet@users.noreply.github.com>
2023-03-25 23:13:47 +0800

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,16 @@
+$('button').click(function(){
+    $('.alert').removeClass("hide");
+    $('.alert').addClass("show");
+    $('.alert').addClass("showAlert");
+    setTimeout(function(){
+        
+        $('.alert').addClass("hide");
+        $('.alert').removeClass("show");
+
+    },5000); //hide alert automatically after 5sec
+});
+
+$('.close-btn').click(function(){
+    $('.alert').addClass("hide");
+    $('.alert').removeClass("show");
+});
